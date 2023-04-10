9e231ad36cd487f4bc98d8ae05363e9813a8f0f1
bohdan200419 <114943793+bohdan200419@users.noreply.github.com>
2023-04-10 23:57:07 +0300

    Update Лабораторна №1.cpp

--- Лабораторна №1/Лабораторна №1.cpp
+++ Лабораторна №1/Лабораторна №1.cpp
@@ -19,24 +19,24 @@ public:
 };
 
 int main() {
-    vector<Employee*> employees; // контейнер з працівниками
+    vector<Employee*> employees; 
     double totalCosts = 0.0; 
 
-    // генеруємо випадкові значення для працівників і додаємо їх до контейнера
+    
     for (int i = 0; i < 10; i++) {
         double hw = rand() % 81 + 20; 
-        double hr = (rand() % 21 + 10) / 10.0; // випадкова погодинна оплата в діапазоні від 1.0 до 3.0
+        double hr = (rand() % 21 + 10) / 10.0; 
         employees.push_back(new Employee(hw, hr));
     }
 
-    // обчислюємо сумарні витрати компанії на оплату праці
+    
     for (int i = 0; i < employees.size(); i++) {
         totalCosts += employees[i]->getSalary();
     }
 
     cout << "Total costs: $" << totalCosts << endl;
 
-    // видаляємо працівників з пам'яті
+   
     for (int i = 0; i < employees.size(); i++) {
         delete employees[i];
     }
